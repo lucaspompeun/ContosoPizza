9e8aa4d02015db56a43cf8f2771c9a1a4cc391cf
Lucas Pompeu Neves <lucaspompeuneves@gmail.com>
2021-06-04 17:47:45 -0300

    Adição de PUT, POST e DELETE

--- Controllers/PizzaController.cs
+++ Controllers/PizzaController.cs
@@ -20,12 +20,51 @@ namespace ContosoPizza.Controllers
             return PizzaService.GetAll();
         }
 
-        // GET by id
+        [HttpGet("{id}")]
+        public ActionResult<Pizza> Get(int id)
+        {
+            var pizza = PizzaService.Get(id);
 
-        // POST
+            if (pizza is null)
+                return NotFound();
 
-        // PUT
+            return pizza;
+        }
 
-        // DELETE
+        [HttpPost]
+        public IActionResult Create(Pizza pizza)
+        {
+            PizzaService.Add(pizza);
+
+            return CreatedAtAction(nameof(Create), new { id = pizza.Id }, pizza);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Pizza pizza)
+        {
+            if (id != pizza.Id)
+                return BadRequest();
+
+            var existingPizza = PizzaService.Get(id);
+            if (existingPizza is null)
+                return NotFound();
+
+            PizzaService.Update(pizza);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var pizza = PizzaService.Get(id);
+
+            if (pizza is null)
+                return NotFound();
+            
+            PizzaService.Delete(id);
+
+            return NoContent();
+        }
     }
 }
